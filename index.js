7c9858d7dfcb46ddedbf688a44424efe9b30099b
kost90 <kostdonc90@gmail.com>
2023-02-28 13:27:31 +0000

    created index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const name = prompt('What is your name?');
+const age = +prompt('What is your age?');
+const gender = prompt('What is your gender?');
+const userDetails = `${name} ${age} ${gender}`;
+alert(`Hello ${userDetails}!`);
+
+let numbers = '12345'
+let arr = numbers.split('');
+console.log(arr [0], arr [1],arr [2], arr [3], arr [4]);
+// alert(arr [0], arr [1], arr [2], arr [3], arr [4]);
